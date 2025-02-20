096f76c2532cf55b87a562570420dbedf622261e
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-02-20 11:44:24 -0600

    chore: Update font and layout in page.tsx and next.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-    </div>
+    <section className="flex flex-col items-center justify-items-center min-h-svh p-8 pb-20 gap-16 font-[family-name:var(--font-jost)]">
+      <h1 className="text-2xl">DOKKAN BATTLE</h1>
+    </section>
   );
 }
