44b6f1f20a6aea875408aa7585e73e3e0bd24306
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-03-20 12:55:57 -0600

    chore: Update header design and add lazy loading for logo image

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,13 @@
 import { ColorModeButton } from "@components/ui/color-mode";
+import Image from "next/image"
 
 export default function Header() {
     return (
-        <header className="flex justify-between bg-[--chakra-colors-bg-subtle] items-center w-full p-4 border-b-[1px] border-b-[--chakra-colors-border-emphasized]">
-            <h1 className="text-2xl text-[--chakra-colors-bg-inverted] font-bold">DOKKAN BATTLE</h1>
+        <header className="fixed top-0 z-10 flex justify-between bg-[linear-gradient(45deg,var(--chakra-colors-bg),var(--chakra-colors-bg-emphasized))] items-center w-full px-4 py-2  border-b-[1px] border-b-[--chakra-colors-border-emphasized] overflow-hidden">
+            <h1 className="text-xl text-[--chakra-colors-bg-inverted] font-bold pl-14">
+                DOKKAN BATTLE
+            </h1>
+            <Image className="absolute -left-10 opacity-100" style={{ filter: "invert(1)" }} src="/logo.png" width={120} height={120} alt="Logo de Goku" />
             <nav className="flex gap-4">
             </nav>
             <ColorModeButton />
